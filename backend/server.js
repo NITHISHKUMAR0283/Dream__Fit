ca8bc1924f4ca6a67f5d76003160e390d7dd8d7f
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2026-03-06 20:27:41 +0530

    initialising folder strucutre for riaynshbaba , created server.js and initialised npm

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv")
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000
+app.use("/",(req,res)=>{
+    res.status(200).json({
+        message: "hello world",
+        success:true
+    })
+})
+app.listen(port,()=>{
+    console.log(`server listening in port ${port}`);
+   
+});
