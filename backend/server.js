cb927245a717496c45fc3e79154d277614fe67b9
NITHISHKUMAR0283 <nk0283@srmist.edu.in>
2026-03-07 05:04:17 +0530

    added projectmodel and installed mongoose:

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv")
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
 app.use("/",(req,res)=>{
     res.status(200).json({
         message: "hello world",
---
+++ backend/src/model/projectModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    //price, description , size(and its unit ) , color , about this item , picture
+    description:{
+        type:String,
+        required:true
+    },
+    sizes:{
+        type:String,
+        required:true        
+    },
+    colors:{
+        type:String,
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    About:{
+        type:String,
+        required:true
+    }
+
+});
+module.exports = mongoose.model("Product",productSchema);
+
