791400a58f3fecf2cdc765f711b25be12b5bdd8f
NithishkumarA <nk0283@srmist.edu.in>
2026-03-20 21:38:24 +0530

    dfdfffddf

--- admin-frontend/vite.config.js
+++ admin-frontend/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from "vite";
 
+import { defineConfig } from 'vite';
 export default defineConfig({
-  server: {
-    port: 5174
-  }
+  base: '/Riyanshbaba/', // Replace with your repo name
+  // ...other config
 });
